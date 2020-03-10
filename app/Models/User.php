0a68308b74006e92746c65d88e3b1e9c3b82c6f3
huxiaoyu <498797574@qq.com>
2020-03-10 02:03:36 +0000

    管理员可以管理权限和角色

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,34 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->hasMany(Reply::class);
     }
 
+    /**
+     * Eloquent 修改器
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if (!\Str::startsWith($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
     public function isAuthorOf($model)
     {
         return $this->id == $model->user_id;
@@ -82,29 +110,5 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->notification_count = 0;
         $this->save();
         $this->unreadNotifications->markAsRead();
-    }
-
-    public function setPasswordAttribute($value)
-    {
-        // 如果值的长度等于 60，即认为是已经做过加密的情况
-        if (strlen($value) != 60) {
-
-            // 不等于 60，做密码加密处理
-            $value = bcrypt($value);
-        }
-
-        $this->attributes['password'] = $value;
-    }
-
-    public function setAvatarAttribute($path)
-    {
-        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
-        if (!\Str::startsWith($path, 'http')) {
-
-            // 拼接完整的 URL
-            $path = config('app.url') . "/uploads/images/avatars/$path";
-        }
-
-        $this->attributes['avatar'] = $path;
     }
 }
