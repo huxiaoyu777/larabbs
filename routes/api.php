9e5579dbee486aa7b1c39bf9c289c0b976a3175c
huxiaoyu <498797574@qq.com>
2020-03-13 07:20:42 +0000

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
-    // 用户注册
-    Route::post('users', 'usersController@store')
-        ->name('users.store');
-});
+Route::prefix('v1')->namespace('Api')
+    ->name('api.v1.')
+    ->middleware('throttle:' . config('api.rate_limits.sign'))
+    ->group(function () {
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')
+            ->name('verificationCodes.store');
+        // 用户注册
+        Route::post('users', 'usersController@store')
+            ->name('users.store');
+    });
