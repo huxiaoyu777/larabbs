bd46f848a5f2a979d76dd14cb48b972cd9c975d2
huxiaoyu <498797574@qq.com>
2020-03-23 07:26:54 +0000

    分类列表

---
+++ app/Http/Controllers/Api/CategoriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+
+class CategoriesController extends Controller
+{
+    public function index()
+    {
+        CategoryResource::wrap('data');
+        return CategoryResource::collection(Category::all());
+    }
+}
