0c95d2029563b3bcb7153167faa8e771d117d8b9
huxiaoyu <498797574@qq.com>
2020-03-23 06:41:06 +0000

    编辑用户信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\UserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Models\Image;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 
@@ -44,5 +45,20 @@ class UsersController extends Controller
     public function me(Request $request)
     {
         return (new UserResource($request->user()))->showSensitiveFields();
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $request->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return (new UserResource($user))->showSensitiveFields();
     }
 }
