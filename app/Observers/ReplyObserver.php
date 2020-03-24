4121e40f7e88918e30e016db54f0ed5c71aa7a6e
huxiaoyu <498797574@qq.com>
2020-03-24 09:53:19 +0000

    消息通知统计

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -18,7 +18,6 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         $reply->topic->updateReplyCount();
-
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
     }
